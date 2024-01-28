10c637e9a52fd9af7c709761e542e1590203e4e6
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-28 20:56:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 //your JS code here. If required.
-const name = document.getElementById("name");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const confirmPassword = document.getElementBydId("confirm-password");
-const submit = document.getElementById("submit");
 
 
-submit.addEventListener("click", (event)=>{
-	
-})
+//Form to extract data
+let form = document.getElementById("signup-form");
+
+// Checking pass validity on btn click
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let p1 = document.getElementById("password");
+  let p2 = document.getElementById("confirm-password");
+
+  if (p1.value !== p2.value) {
+    alert("Passwords do not match");
+  } else {
+    let elements = form.children;
+    Array.from(elements).forEach((val) => {
+      if (
+        val.hasAttribute("id") &&
+        val.id != "confirm-password" &&
+        val.id != "submit"
+      ) {
+        window.sessionStorage.setItem(val.id, val.value);
+		
+      }
+	  val.value = "";
+    });
+
+    alert("Sign up successfull");
+  }
+});
