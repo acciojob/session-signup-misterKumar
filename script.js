1c0932a9907c53820e770ed6d9d931d2a3818256
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-28 20:43:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const submitBtn = document.getElementById("submit");
+
+submitBtn.addEventListener('click',()=>{
+const name = document.querySelector('#name').value;
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+const confirmPassword = document.getElementById("confirm-password").value;
+	if(password === confirmPassword){
+		window.sessionStorage.setItem("name",name);
+		window.sessionStorage.setItem("email",email);
+		window.sessionStorage.setItem("password",password);
+		alert("Sign up successful!.")	
+	}
+	else{
+		alert("Passwords do not match");
+	}
+})
